80b6a76205f881de1a5330011c2aef6bcae69790
JacobHdez <jacohv98@gmail.com>
2020-10-21 14:17:59 -0500

    Starting application

---
+++ Ze_Puppet/src/Application.cpp
@@ -0,0 +1,53 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* ---------- Code ---------- */
+    if (glewInit() != GLEW_OK)
+        std::cout << "[-] Error glewInit()" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+    /* ---------- End ---------- */
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* ---------- Code ---------- */
+        /* ---------- End ---------- */
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    /* ---------- Code ---------- */
+    /* ---------- End ---------- */
+
+    glfwTerminate();
+    return 0;
+}
