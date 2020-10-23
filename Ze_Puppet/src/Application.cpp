dd76cdbdc5f4308a3d2f7e7aa7d7995038e35e45
JacobHdez <jacohv98@gmail.com>
2020-10-23 13:49:32 -0500

    Error handling

--- Ze_Puppet/src/Application.cpp
+++ Ze_Puppet/src/Application.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 
+#include "Renderer.h"
+
 int main(void)
 {
     GLFWwindow* window;
@@ -28,25 +30,27 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
     /* ---------- End ---------- */
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(window))
+        {
+            /* Render here */
+            GLCall(glClear(GL_COLOR_BUFFER_BIT));
+
+            /* ---------- Code ---------- */
+            /* ---------- End ---------- */
+
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
 
         /* ---------- Code ---------- */
         /* ---------- End ---------- */
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
     }
-
-    /* ---------- Code ---------- */
-    /* ---------- End ---------- */
 
     glfwTerminate();
     return 0;
