fe44d4ec8e8b6cc940db052b7c5b8306c442ab80
JacobHdez <jacohv98@gmail.com>
2020-10-24 13:14:48 -0500

    Basic camera test

--- Ze_Puppet/src/Application.cpp
+++ Ze_Puppet/src/Application.cpp
@@ -11,53 +11,115 @@
 #include "VertexArray.h"
 #include "Shader.h"
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+GLFWwindow* Init_GLFW();
+
 int main(void)
+{
+    GLFWwindow* window = Init_GLFW();
+    if (window == nullptr)
+        return -1;
+
+    {
+        /* ---------- TEST ---------- */
+        float positions[] = {
+            -0.5f, -0.5f, // 0
+             0.5f, -0.5f, // 1
+             0.5f,  0.5f, // 2
+            -0.5f,  0.5f, // 3
+        };
+
+        unsigned int indices[] = {
+            0, 1, 2,
+            2, 3, 0
+        };
+
+        VertexArray va;
+        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        va.AddBuffer(vb, layout);
+
+        /* ---------- Camera ---------- */
+        glm::mat4 Projection = glm::perspective(glm::pi<float>() * 0.25f, 4.0f / 3.0f, 0.1f, 100.f);
+        glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -1.0f));
+        View = glm::rotate(View, 2.0f, glm::vec3(-1.0f, 0.0f, 0.0f));
+        View = glm::rotate(View, -2.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::mat4 Model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
+
+        glm::mat4 Result = Projection * View * Model;
+        /* ---------- End ---------- */
+
+        IndexBuffer ib(indices, 6);
+
+        Shader shader("res/shaders/Basic.shader");
+        shader.Bind();
+        shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+        shader.SetUniformMat4f("u_MVP", Result);
+
+        va.Unbind();
+        vb.Unbind();
+        ib.Unbind();
+        shader.Unbind();
+
+        Renderer renderer;
+
+        float r = 0.0f;
+        float increment = 0.05f;
+
+        while (!glfwWindowShouldClose(window))
+        {
+            renderer.Clear();
+
+            shader.Bind();
+            shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+
+            renderer.Draw(va, ib, shader);
+
+            if (r > 1.0f)
+                increment = -0.05f;
+            else if (r < 0.0f)
+                increment = 0.05f;
+
+            r += increment;
+
+
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+GLFWwindow* Init_GLFW()
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
     if (!glfwInit())
-        return -1;
+        return nullptr;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(512, 512, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
-        return -1;
+        return nullptr;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* ---------- Code ---------- */
+    glfwSwapInterval(5);
+
+    /* ---------- GLEW ---------- */
     if (glewInit() != GLEW_OK)
         std::cout << "[-] Error glewInit()" << std::endl;
 
     std::cout << glGetString(GL_VERSION) << std::endl;
     /* ---------- End ---------- */
-    {
 
-        /* Loop until the user closes the window */
-        while (!glfwWindowShouldClose(window))
-        {
-            /* Render here */
-            GLCall(glClear(GL_COLOR_BUFFER_BIT)); // renderer.Clear()
-
-            /* ---------- Code ---------- */
-            /* ---------- End ---------- */
-
-            /* Swap front and back buffers */
-            glfwSwapBuffers(window);
-
-            /* Poll for and process events */
-            glfwPollEvents();
-        }
-
-        /* ---------- Code ---------- */
-        /* ---------- End ---------- */
-    }
-
-    glfwTerminate();
-    return 0;
+    return window;
 }
