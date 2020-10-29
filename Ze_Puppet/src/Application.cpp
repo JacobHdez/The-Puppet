4ab85b7eb6a3cb44bb89662e92ec76e05fa3e44b
JacobHdez <jacohv98@gmail.com>
2020-10-28 20:49:04 -0600

    Rearrangement of libraries

--- Ze_Puppet/src/Application.cpp
+++ Ze_Puppet/src/Application.cpp
@@ -5,20 +5,20 @@
 
 #include "Renderer.h"
 
-#include "VertexBuffer.h"
+/*#include "VertexBuffer.h"
 #include "VertexBufferLayout.h"
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
 
+#include "Util/Camera.h"
+
 #include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/matrix_transform.hpp"*/
 
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
-
-#include "Util/Camera.h"
 
 #include "tests/TestClearColor.h"
 
