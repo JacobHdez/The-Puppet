f6ed1b668087894dcaf4bea3eac0fb0f8ed2097b
JacobHdez <jacohv98@gmail.com>
2020-11-04 10:14:20 -0600

    Added SceneNode class

---
+++ Ze_Puppet/src/OpenGL-Core/SceneNode.cpp
@@ -0,0 +1,52 @@
+#include "SceneNode.h"
+
+SceneNode::SceneNode()
+	: m_Parent(nullptr), m_Mesh(nullptr),
+	  m_WorldTransform{ 1.0f }, m_Transform{ 1.0f }, m_ModelScale{ 1.0f },
+	  m_Colour{ 0.7f, 0.7f, 0.7f }
+{
+}
+
+SceneNode::SceneNode(Mesh* mesh, glm::vec3 colour)
+	: m_Parent(nullptr),
+	m_WorldTransform{ 1.0f }, m_Transform{ 1.0f }, m_ModelScale{ 1.0f }
+{
+	m_Mesh = mesh;
+	m_Colour = colour;
+}
+
+SceneNode::~SceneNode()
+{
+	for (unsigned int i = 0; i < m_Children.size(); ++i)
+	{
+		delete m_Children[i];
+	}
+}
+
+void SceneNode::AddChild(SceneNode* sn)
+{
+	m_Children.push_back(sn);
+	sn->m_Parent = this;
+}
+
+void SceneNode::OnUpdate(float deltaTime)
+{
+	if (m_Parent)
+	{
+		m_WorldTransform = m_Parent->m_WorldTransform * m_Transform;
+	}
+	else
+	{
+		m_WorldTransform = m_Transform;
+	}
+
+	for (auto& it : m_Children)
+	{
+		it->OnUpdate(0.0f);
+	}
+}
+
+void SceneNode::Draw(Shader& shader)
+{
+	m_Mesh->Draw(shader);
+}
---
+++ Ze_Puppet/src/OpenGL-Core/SceneNode.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "Mesh.h"
+#include "glm/glm.hpp"
+#include <vector>
+
+class SceneNode
+{
+protected:
+	SceneNode* m_Parent;
+	Mesh* m_Mesh;
+	glm::mat4 m_WorldTransform;
+	glm::mat4 m_Transform;
+	glm::vec3 m_ModelScale;
+	glm::vec3 m_Colour;
+	std::vector<SceneNode*> m_Children;
+public:
+	SceneNode();
+	SceneNode(Mesh* mesh, glm::vec3 colour);
+	~SceneNode();
+
+	void SetTransform(const glm::mat4& transform) { m_Transform = transform; }
+	const glm::mat4& GetTransform() const { return m_Transform; }
+	glm::mat4 GetWorldTransform() const { return m_WorldTransform; }
+
+	glm::vec3 GetColour() const { return m_Colour; }
+	void SetColour(glm::vec3 colour) { m_Colour = colour; }
+
+	glm::vec3 GetModelScale() const { return m_ModelScale; }
+	void SetModelScale(glm::vec3 scale) { m_ModelScale = scale; }
+
+	Mesh* GetMesh() const { return m_Mesh; }
+	void SetMesh(Mesh* mesh) { m_Mesh = mesh; }
+
+	void AddChild(SceneNode* sn);
+
+	void OnUpdate(float deltaTime);
+	void Draw(Shader& shader);
+
+	/*std::vector<SceneNode*>::const_iterator GetChildIteratorBegin() { return m_Children.begin(); }
+	std::vector<SceneNode*>::const_iterator GetChildIteratorEnd() { return m_Children.end(); }*/
+};
